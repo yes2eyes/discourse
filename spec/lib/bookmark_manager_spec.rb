141c40eb08845f39d9d904dc175d32309aaf0f93
Martin Brennan <martin@discourse.org>
2020-03-04 15:09:42 +1100

    Allow users to select custom date + time for bookmark reminders
    * re-enable option in UI and add date + time picker
    * add validation and warning for custom date + time
    * fix some server-side issues with deleting post bookmark and deleting
      topic bookmarks

--- spec/lib/bookmark_manager_spec.rb
+++ spec/lib/bookmark_manager_spec.rb
@@ -105,8 +105,8 @@ RSpec.describe BookmarkManager do
 
   describe ".destroy_for_topic" do
     let!(:topic) { Fabricate(:topic) }
-    let(:bookmark1) { Fabricate(:bookmark, topic: topic, post: Fabricate(:post, topic: topic), user: user) }
-    let(:bookmark2) { Fabricate(:bookmark, topic: topic, post: Fabricate(:post, topic: topic), user: user) }
+    let!(:bookmark1) { Fabricate(:bookmark, topic: topic, post: Fabricate(:post, topic: topic), user: user) }
+    let!(:bookmark2) { Fabricate(:bookmark, topic: topic, post: Fabricate(:post, topic: topic), user: user) }
 
     it "destroys all bookmarks for the topic for the specified user" do
       subject.destroy_for_topic(topic)
